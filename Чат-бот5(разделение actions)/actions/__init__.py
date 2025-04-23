9ccdf4fe19c34cad4b154e9ec58d07d75420fcdf
SonyaKotova <118314090+SonyaKotova@users.noreply.github.com>
2025-04-23 15:19:58 +0300

    Add files via upload

---
+++ Чат-бот5(разделение actions)/actions/__init__.py
@@ -0,0 +1,15 @@
+from .weather import ActionWeather
+from .translate import ActionTranslate
+from .calculate import ActionCalculate
+from .datetime import ActionDatetime
+from .search import ActionSearch
+from .sentiment import ActionAnalyzeSentiment
+
+__all__ = [
+    'ActionWeather',
+    'ActionTranslate',
+    'ActionCalculate',
+    'ActionDatetime',
+    'ActionSearch',
+    'ActionAnalyzeSentiment'
+]
